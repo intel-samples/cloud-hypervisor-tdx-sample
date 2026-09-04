c44de53f0bc313a2a6753d89f1c82e966c10e640
Liang, Zhou <liang1.zhou@intel.com>
2026-09-04 20:15:29 +0530

    tdx: mask CPUID.0xD using TDX supported_xfam
    Signed-off-by: Liang, Zhou <liang1.zhou@intel.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -1331,6 +1331,9 @@ impl Vmm {
                     tdx: false,
                     amx,
                 },
+                #[cfg(feature = "tdx")]
+                // Live Migration is not supported when TDX is enabled
+                None,
             )
             .map_err(|e| {
                 MigratableError::MigrateSend(anyhow!("Error generating common cpuid': {e:?}"))
@@ -1440,6 +1443,9 @@ impl Vmm {
                     tdx: false,
                     amx: vm_config.cpus.features.amx,
                 },
+                #[cfg(feature = "tdx")]
+                // Live Migration is not supported when TDX is enabled
+                None,
             )
             .map_err(|e| {
                 MigratableError::MigrateReceive(anyhow!("Error generating common cpuid: {e:?}"))
